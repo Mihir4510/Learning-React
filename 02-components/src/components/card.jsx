13034cf0e38d33ec874b1861aad73ff1c982b62f
Mihir Parmar <mihirparmar766@gmail.com>
2026-02-19 18:04:24 +0530

    components based architecture

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    
+      <div className='card'>
+        <h1>mihir parmar</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum molestias earum eum minima aperiam illum distinctio officia veritatis similique voluptas aliquam eveniet quos exercitationem, consectetur sit excepturi. Atque, quod odit.</p>
+      </div>
+  )
+}
+
+export default card
