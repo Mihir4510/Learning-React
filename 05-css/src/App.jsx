60f1a971dba0009f9f35d93bf5151af968c5afe8
Mihir Parmar <mihirparmar766@gmail.com>
2026-02-22 16:48:12 +0530

    css with react

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './components/button/Button.jsx'
+
+import Header from './components/header/Header.jsx'
+
+const App = () => {
+  return (
+   
+    <>
+
+    <Header/>
+    <Button/>
+    
+     
+    </>
+  )
+}
+
+export default App
